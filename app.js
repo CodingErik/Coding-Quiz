6d60d33dd9b0c32d8228944e40c735c8157242ca
Erik De Luna <codingerik1.gmail.com>
2020-05-12 04:40:20 -0500

    basic timer is working, loaded a few quesions, working on button target

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ let startCard = document.getElementById('startCard')
 let startButton = document.getElementById('startButton');
 
 // question card
-let questionCard = document.getElementById('questionCard')
+let questionCard = document.getElementById('questionCard');
 let answerButtons = document.querySelector('.answerButtons')
+let questionButtons = document.getElementById('questionButtons');
 
 // scorecard
 let scoreCard = document.getElementById('scoreCard')
@@ -12,33 +13,80 @@ let scoreCard = document.getElementById('scoreCard')
 
 // total time
 let mins = 3;
-let totalSeconds = 10000;
+let totalSeconds = 60;
 
 // mins * 60;
 
 // timer
-let interval; 
+let interval;
+
+let question =  {
+
+    question1: { question:'how many wood can you eat',
+                answer1:'i can eat about 1',
+                answer1:'i can eat about 2',
+                answer1:'i can eat about 3'
+            },
+    question2: { question:'how many wood can you eat',
+                answer1:'i can eat about 1',
+                answer1:'i can eat about 2',
+                answer1:'i can eat about 3'
+            },
+    question3: { question:'how many wood can you eat',
+                answer1:'i can eat about 1',
+                answer1:'i can eat about 2',
+                answer1:'i can eat about 3'
+            }
+}
+
+
+// this is the start button 
+// startButton.addEventListener('click', function(e){
+//     // displaying the questions card, hiding the stardCard
+//     startCard.setAttribute('class', 'd-none');
+//     questionCard.classList.remove("d-none");
+
+
+//     console.log('start button is being pressed');
+
+//     // start the timer 
+//     interval = setInterval(gameTimer, 1000);
+// });
 
 
 
+// timer function, TIMER STARTS
+function gameTimer() {
+    console.log(totalSeconds);
+    totalSeconds--;
+
+    if (totalSeconds === 0) {
+        clearInterval(interval);
+        console.log('test is done');
+        gameEnds();
+    }
+
+};
 
 
-startButton.addEventListener('click', function(){
-    startCard.setAttribute('class', 'd-none');
-    questionCard.classList.remove("d-none");
 
-    // start the timer 
-    interval = setInterval(gameTimer, 1000);
+questionButtons.addEventListener('click', function (event) {
+    // make a new question appear 
+    // deduct or add points to the score
+    if (event.target.matches('button').id === 'correct') {
+        console.log('This is the wrong answer');
+
+        
+    }
 });
 
 
-function gameTimer() {
-  console.log('hello');
-  totalSeconds--;
-  if(totalSeconds === 0){
-      clearInterval(interval);
-      console.log('test is done')
-  }
 
-}; 
 
+
+
+// game ENDS and display the scorecard
+function gameEnds() {
+    questionCard.setAttribute('class', 'd-none');
+    scoreCard.classList.remove("d-none");
+};
